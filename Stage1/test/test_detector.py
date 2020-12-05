33dee227679f97849b3a3871eeec3b0b70c5d9f2
MjH.Sakh <48555070+mjh-sakh@users.noreply.github.com>
2020-12-05 21:55:44 +1100

    test(environment): Env2d init for image

--- Stage1/test/test_detector.py
+++ Stage1/test/test_detector.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 
 from perception.detector import Detector
+from environment.environment2d import Env2D
 
 
 class TestDetector(unittest.TestCase):
@@ -27,6 +28,24 @@ class TestDetector(unittest.TestCase):
         self.assertEqual(keypoints[0].pt[1], 0)
         np.testing.assert_array_equal(descriptors[0], np.array([100] * 3 + [200] * 3))
 
+class TestEnvironment(unittest.TestCase):
+    """
+    Tests for Env2D
+    """
+
+    def setUp(self) -> None:
+        # 20x20 px, outer pixel frame is pure green, next is black, rest is white
+        self.test_map_path = "test_map.bmp"
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_Env2d_init_with_image(self):
+        env = Env2D(self.test_map_path)
+
+        self.assertEqual(env.plan_file_path, self.test_map_path)
+        self.assertEqual(env.plan_type, "image")
+        self.assertEqual(env.plan.shape, (20, 20, 3))
 
 if __name__ == '__main__':
     unittest.main()
